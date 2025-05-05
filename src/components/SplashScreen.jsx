d6fe4021736502bf1261b889585ce73f6b29c094
Callylove <nwekechidimma88@gmail.com>
2025-05-05 16:25:08 +0100

    added a splash screen

---
+++ src/components/SplashScreen.jsx
@@ -0,0 +1,82 @@
+
+import React, { useState, useEffect } from 'react';
+import {  AnimatePresence } from 'framer-motion';
+import '../styles/SplashScreen.css'
+import Logo from '../assets/logo.png'
+const SplashScreen = () => {
+  const [isLoading, setIsLoading] = useState(true);
+
+const rotationSpeed = 5
+  useEffect(() => {
+    // Simulate loading time
+    const timer = setTimeout(() => {
+      setIsLoading(false);
+  
+    }, 30000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
+
+
+  return (
+    <div className="splash-container">
+      <AnimatePresence>
+      {isLoading && (
+          <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+            className="splash-loading-container"
+          >
+            <motion.div
+              animate={{ 
+                rotate: 360,
+                transition: { 
+                  duration: rotationSpeed, 
+                  repeat: Infinity, 
+                  ease: "linear" 
+                }
+              }}
+              className="logo-container"
+            >
+            
+              <img 
+                src={Logo} 
+                alt="Logo" 
+                className="logo-image" 
+              />
+            </motion.div>
+            
+            <motion.div
+              animate={{
+                opacity: [0.4, 1, 0.4],
+                transition: {
+                  duration: 1.5,
+                  repeat: Infinity,
+                }
+              }}
+              className="loading-text"
+            >
+              SEPHCOCCO
+              <motion.span
+                animate={{
+                  opacity: [0, 1, 0],
+                  transition: {
+                    duration: 1.5,
+                    repeat: Infinity,
+                    repeatType: "loop"
+                  }
+                }}
+              >...</motion.span>
+            </motion.div>
+          </motion.div>
+        )}
+       
+      </AnimatePresence>
+    
+    </div>
+  );
+};
+
+export default SplashScreen;
