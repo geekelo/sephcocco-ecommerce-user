849c52d35fd99befa289eca61a8dc502f1c0c18c
Callylove <nwekechidimma88@gmail.com>
2025-05-06 20:25:21 +0100

    Product page Ui

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,7 +1,19 @@
 import React from 'react'
+import SearchFilter from '../components/SearchFilter'
+import Hero from '../components/Hero'
+import '../styles/Product.css'
+import { renderMultipleShowcases } from '../components/RenderMultipleShowCases';
 
 export default function Product() {
+    const filterOptions = ['Price: Low to High', 'Price: High to Low', 'Newest First', 'Popularity', 'Rating'];
   return (
-    <div>Product</div>
+    <>
+      <SearchFilter className='mobile-product-only' filterOptions={filterOptions}/>
+      <Hero/>
+      <section className="product-showcases-container">
+          {renderMultipleShowcases()}
+        </section>
+    </>
+ 
   )
 }
