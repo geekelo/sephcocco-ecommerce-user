7f8ea4378bcea39b299269d61b6363e2353be979
Callylove <nwekechidimma88@gmail.com>
2025-05-06 02:31:56 +0100

    added the outlet screen

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,29 @@
 
 
+import { useState } from 'react';
 import { ErrorBoundary } from './components/ErrorBoundary'
 import SplashScreen from './components/SplashScreen'
+import OutletPage from './pages/Outlet';
 
 function App() {
+  const [loading, setLoading] = useState(true);
 
+  // Complete splash screen and show main content
+  const handleSplashComplete = () => {
+    setLoading(false);
+  };
 return (
-  <ErrorBoundary>
-      <SplashScreen />
-    </ErrorBoundary>
+  <div>
+      {loading && (
+          <ErrorBoundary>
+    <SplashScreen onComplete={handleSplashComplete} />
+          </ErrorBoundary>
+    
+      )}
+      
+      {!loading &&   <ErrorBoundary><OutletPage /></ErrorBoundary> }
+    </div>
+
 )
 }
 
