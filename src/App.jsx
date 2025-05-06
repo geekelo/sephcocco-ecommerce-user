2bbf4c1b9ce62dc37e6d2cfcaa33e0561fbc49bd
Callylove <nwekechidimma88@gmail.com>
2025-05-06 10:45:28 +0100

    implemented the routing system

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,19 @@
 
 
 import { useState } from 'react';
-import { ErrorBoundary } from './components/ErrorBoundary'
-import SplashScreen from './components/SplashScreen'
-import OutletPage from './pages/Outlet';
+import AppRouter from './routes/AppRouter';
+
 
 function App() {
-  const [loading, setLoading] = useState(true);
 
-  // Complete splash screen and show main content
-  const handleSplashComplete = () => {
-    setLoading(false);
-  };
+
+
 return (
-  <div>
-      {loading && (
-          <ErrorBoundary>
-    <SplashScreen onComplete={handleSplashComplete} />
-          </ErrorBoundary>
-    
-      )}
-      
-      {!loading &&   <ErrorBoundary><OutletPage /></ErrorBoundary> }
-    </div>
+  
+   <AppRouter />
+
+
+
 
 )
 }
---
+++ src/routes/AppRouter.jsx
@@ -0,0 +1,41 @@
+import { Suspense, useState, } from "react";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { OutletPage, ProductPage } from "./LazyLoader";
+import HomeLayout from "../layout/homeLayout";
+import {ErrorBoundary} from '../components/ErrorBoundary'
+import { SplashScreen } from "../components/SplashScreen";
+
+
+
+const AppRouter = () => {
+    const [loading, setLoading] = useState(true);
+ 
+    
+
+    const handleSplashComplete = () => {
+      setLoading(false);
+    };
+    
+    return (
+      <BrowserRouter>
+        <ErrorBoundary>
+          {loading ? (
+            <SplashScreen onComplete={handleSplashComplete} />
+          ) : (
+            <Suspense fallback={<SplashScreen />}>
+              <Routes>
+                <Route path="/" element={<OutletPage />} />
+                <Route element={<HomeLayout />}>
+                  <Route path="/product" element={<ProductPage />} />
+                </Route>
+                {/* Redirect unknown routes to home */}
+                <Route path="*" element={<Navigate to="/" replace />} />
+              </Routes>
+            </Suspense>
+          )}
+        </ErrorBoundary>
+      </BrowserRouter>
+    );
+  };
+
+export default AppRouter;
